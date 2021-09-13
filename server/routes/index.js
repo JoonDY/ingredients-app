6edebc65e46a0d739cb8b1ed4872a87558c3b13a
Joon <daniel.joon.yi@gmail.com>
2021-09-12 21:04:50 -0700

    Separate client and server

--- server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const ingredients = require('./ingredientsRoute');
-const user = require('./usersRoute');
-
-module.exports = (app) => {
-  app.use('/api/v1/ingredients', ingredients);
-  app.use('/auth', user);
-};
